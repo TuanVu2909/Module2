95fcebb6b7c9d83bfee24547b9362e961f3f66df
TuanVu0929 <tranngoc.haidang0402@gmail.com>
2023-04-03 20:51:01 +0700

    file mới

--- src/Date0403/DocSoThanhChu.java
+++ src/Date0403/DocSoThanhChu.java
@@ -7,11 +7,49 @@ public class DocSoThanhChu {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập số:");
         int number = scanner.nextInt();
-        int a=number/100;
-        switch (number){
-            case a:
-
+        int a = number/100;
+        int b = (number%100)/10;
+        int c = number%10;
+        String total = "";
+        switch (a){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+              total +=  a+"hundred";
+                break;
         }
-
+        switch (b){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                total +=  b+"ty";
+                break;
+        }
+        switch (c){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                total +=  c+"ghfg";
+                break;
+        }
+        System.out.println(total);
     }
 }
---
+++ src/Date0403/Total20Prime.java
@@ -0,0 +1,28 @@
+package Date0403;
+
+public class Total20Prime {
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+            for (int i = 0; i < 500; i++) {
+                int a =i;
+                for (int j = 2; j <= a; j++) {
+                    if (a == j) {
+                        count++;
+                        sum +=i;
+                    }
+                    if (a %j==0) {
+                        break;
+                }
+
+                }if (count==20){
+                    break;
+                }
+
+            }
+
+        System.out.println(sum);
+    }
+
+}
+
